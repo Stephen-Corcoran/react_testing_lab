215491878a72b2ec299dd771af4457cb2b706696
Stephen <stephendcorcoran@gmail.com>
2021-07-29 08:45:27 +0100

    output as expected for a range of numbers

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -14,4 +14,59 @@ describe("Calculator", () => {
     cy.get(".display").should('contain', '32')
   })
 
+  it("should update the display of the the arithmetical operations", () => {
+    cy.get("#number3").click();
+    cy.get("#operator-multiply").click();
+    cy.get("#number2").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should('contain', '6')
+  })
+  it("should chain multiple operations together", () => {
+    cy.get("#number3").click();
+    cy.get("#operator-multiply").click();
+    cy.get("#number2").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number2").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should('contain', '4')
+  })
+
+  it("should get he output as expected for a range of numbers (positive)", () => {
+  cy.get("#number2").click();
+  cy.get("#operator-multiply").click();
+  cy.get("#number3").click();
+  cy.get("#operator-equals").click();
+  cy.get(".display").should('contain', '6')
+})
+
+it("should get he output as expected for a range of numbers (negative)", () => {
+  cy.get("#number2").click();
+  cy.get("#operator-subtract").click();
+  cy.get("#number3").click();
+  cy.get("#operator-equals").click();
+  cy.get(".display").should('contain', '-1')
+})
+
+it("should get he output as expected for a range of numbers (decimals)", () => {
+  cy.get("#number3").click();
+  cy.get("#operator-divide").click();
+  cy.get("#number2").click();
+  cy.get("#operator-equals").click();
+  cy.get(".display").should('contain', '1.5')
+})
+
+it("should get he output as expected for a range of numbers (very large numbers)", () => {
+  cy.get("#number3").click();
+  cy.get("#number2").click();
+  cy.get("#number3").click();
+  cy.get("#number2").click();
+  cy.get("#operator-multiply").click();
+  cy.get("#number3").click();
+  cy.get("#number2").click();
+  cy.get("#number3").click();
+  cy.get("#number2").click();
+  cy.get("#operator-equals").click();
+  cy.get(".display").should('contain', '10445824')
+})
+
 })
